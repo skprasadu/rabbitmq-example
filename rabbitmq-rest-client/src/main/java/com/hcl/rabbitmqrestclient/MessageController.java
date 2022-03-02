4dd9eb7b6dbf98d6d3e0701bc51d6fe9e338157d
Krishna Prasad <praskrishna@gmail.com>
2022-03-02 13:49:08 -0800

    ignore

---
+++ rabbitmq-rest-client/src/main/java/com/hcl/rabbitmqrestclient/MessageController.java
@@ -0,0 +1,21 @@
+package com.hcl.rabbitmqrestclient;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+	private final RabbitTemplate rabbitTemplate;
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	public MessageController(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	@GetMapping("hello")
+	public void hello() {
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+	}
+}
---
+++ rabbitmq-rest-client/src/main/java/com/hcl/rabbitmqrestclient/RabbitmqRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.rabbitmqrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqRestClientApplication.class, args);
+	}
+
+}
